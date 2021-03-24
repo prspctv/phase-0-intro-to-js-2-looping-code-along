66f0ad9735b5d2e2afd432ad52e0027fb1a6d568
Kalin Jack <75442486+prspctv@users.noreply.github.com>
2021-03-24 08:41:27 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,40 @@
 // Code your solutions in this file
 
-function writeCards(nameArr, event) {
-    let newArr = []
-    for (let i = 0; i < nameArr.length; i++) {
-    newArr.push(`Thank you, ${nameArr[i]}, for the wonderful ${event} gift!`);
-    
-  }
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+  } 
+  
+const gifts = ["teddy bear", "drone", "doll"];
 
-  return newArr;
-}
-
-function countDown(num) {
-    let i = 0
-    while (num >= 0){
-        console.log(num--)
+  function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+      console.log(`Wrapped ${gifts[i]} and added a bow!`);
     }
+  
+    return gifts;
+  }
+  
+  wrapGifts(gifts);
+
+
+function writeCards(arr, str) {
+    let newArr = []
+
+    for (let i = 0; i < arr.length; i++) {
+        newArr.push (`Thank you, ${arr[i]}, for the wonderful ${str} gift!`);
+    }
+    return newArr
 }
+
+    writeCards(["Lisa", "Kaitlin", "Jan"], "surprise"); 
+
+
+function countDown(i) {
+    i = 10
+    while (i >= 0) {
+        console.log(i--);
+    } 
+}
+
+    countDown();
+
